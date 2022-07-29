fa9d23990c1a59eb25d677434039ff0d25946341
José Carlos <a01174050@tec.mx>
2022-07-29 13:02:28 -0500

    All the functions needed are done

--- main.cpp
+++ main.cpp
@@ -5,35 +5,53 @@ void add_word(std::vector <char>& letras,char letra)
 {
     letras.push_back(letra);
 }
-
+void change_incognito(std::vector <char>& incognita,char letra, std::string palabra)
+{
+    for (int i = 0; i < incognita.size(); i++)
+    {
+        if (palabra[i]==letra)
+        {
+            incognita[i]=letra;
+        }
+    }
+}
+void show_incognito(std::vector <char>& incognita)
+{
+    for (int i = 0; i < incognita.size(); i++)
+    {
+        std::cout<<incognita[i]<<" ";
+    }
+    std::cout<<"\n";
+}
+void show_letras_usadas(std::vector <char>& letras_usadas)
+{
+    for (int i = 0; i < letras_usadas.size(); i++)
+    {
+        std::cout<<letras_usadas[i]<<" ";
+    }
+    std::cout<<"\n";
+}
 int main(){
 
     std::string Palabras[3]= {"autos","perros","animales"};
     std::vector <char> letras_usadas;
-    letras_usadas.push_back('a');
+    std::vector <char> palabra_incognita;
+    
     for (auto &&i : Palabras[0])
     {
         std::cout<<i<<" ";
-    }
-    std::cout<<Palabras[0].length()<<"\n";
-    
+    }    
+    int i=0;
     for (int i = 0; i < Palabras[0].length(); i++)
     {
-        std::cout<<"_ ";
+        palabra_incognita.push_back('_');
     }
+    
+    
     char letra;
-    for (int i = 0; i < 2; i++)
-    {
-        std::cin>>letra;
-        add_word(letras_usadas,letra);
-    }
-    for (auto &&i : letras_usadas)
-    {
-        std::cout<<i<<" ";
-        std::cout<<"\n";
-    }
+
     
-    for (int i = 0; i < Palabras[0].length(); i++)
+    /*for (int i = 0; i < Palabras[0].length(); i++)
     {
         if (Palabras[0][i]==letras_usadas.back())
         {
@@ -44,7 +62,20 @@ int main(){
             std::cout<<"_ ";
             
         }   
-    }
+    }*/
+    /*show_incognito(palabra_incognita);*/
+    std::cout<<"Write a letter"<<std::endl;
+    std::cin>>letra;
+    add_word(letras_usadas,letra);
+    change_incognito(palabra_incognita,letras_usadas.back(),Palabras[0]);
+    show_incognito(palabra_incognita);
+    std::cout<<"Write a letter"<<std::endl;
+    std::cin>>letra;
+    add_word(letras_usadas,letra);
+    change_incognito(palabra_incognita,letras_usadas.back(),Palabras[0]);
+    show_incognito(palabra_incognita);
+    show_letras_usadas(letras_usadas);
+    
     
     
     
